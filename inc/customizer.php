4a98e9d9e40ee0828b69483419b3d8888d2c2093
Sami Keijonen <sami.keijonen@foxnet.fi>
2017-03-05 20:41:34 +0200

    Check that EDD is active before setting partial refresh for it's features.

--- inc/customizer.php
+++ inc/customizer.php
@@ -19,13 +19,15 @@ function checathlon_plus_customize_register( $wp_customize ) {
 
 	if ( function_exists( 'EDD' ) ) {
 		require_once( checathlon_plus()->dir_path . 'inc/customizer/settings-edd.php' );
+
+		// PostMessage settings for EDD.
+		$wp_customize->get_setting( 'empty_cart_text' )->transport               = 'postMessage';
+		$wp_customize->get_setting( 'downloads_featured_area_title' )->transport = 'postMessage';
 	}
 
 	// PostMessage settings.
 	$wp_customize->get_setting( 'before_footer_area_title' )->transport      = 'postMessage';
 	$wp_customize->get_setting( 'footer_text' )->transport                   = 'postMessage';
-	$wp_customize->get_setting( 'empty_cart_text' )->transport               = 'postMessage';
-	$wp_customize->get_setting( 'downloads_featured_area_title' )->transport = 'postMessage';
 
 	// Set partial refresh.
 	if ( isset( $wp_customize->selective_refresh ) ) {
@@ -46,21 +48,23 @@ function checathlon_plus_customize_register( $wp_customize ) {
 			},
 		) );
 
-		// Partial refresh for empty cart text.
-		$wp_customize->selective_refresh->add_partial( 'empty_cart_text', array(
-			'selector'            => '.edd_empty_cart',
-			'render_callback'     => function() {
-				return checathlon_get_empty_cart_text_html();
-			},
-		) );
+		if ( function_exists( 'EDD' ) ) {
+			// Partial refresh for empty cart text.
+			$wp_customize->selective_refresh->add_partial( 'empty_cart_text', array(
+				'selector'            => '.edd_empty_cart',
+				'render_callback'     => function() {
+					return checathlon_get_empty_cart_text_html();
+				},
+			) );
 
-		// Partial refresh for downloads featured area title.
-		$wp_customize->selective_refresh->add_partial( 'downloads_featured_area_title', array(
-			'selector'            => '.downloads-featured-title',
-			'render_callback'     => function() {
-				return checathlon_get_downloads_featured_area_title_html();
-			},
-		) );
+			// Partial refresh for downloads featured area title.
+			$wp_customize->selective_refresh->add_partial( 'downloads_featured_area_title', array(
+				'selector'            => '.downloads-featured-title',
+				'render_callback'     => function() {
+					return checathlon_get_downloads_featured_area_title_html();
+				},
+			) );
+		}
 
 	}
 
